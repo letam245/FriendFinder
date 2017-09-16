2b1a36353e36eec3381b03f4ec8c1688813cb988
tammy le <tammyle@tammys-MBP.attlocal.net>
2017-09-16 03:23:55 -0700

    server change

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+// express middleware needed for serving static files. 
+app.use(express.static(__dirname + '/app/public'));
+
+// bring in api routes
+require("./app/routing/api_routes.js")(app);
+require("./app/routing/html_routes.js")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
